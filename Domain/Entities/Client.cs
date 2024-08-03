c0d57fcc5f84d1a636042d533770ff589e300b9d
JosseGonnza <accion.quevedo@hotmail.com>
2024-08-03 20:01:09 +0100

    Create the base domain entity and the client entity.
    We create AuditableBaseEntity as an entity model and make the client entity inherit from it. We create the client properties and handle the logic to calculate the client age.

---
+++ Domain/Entities/Client.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities
+{
+    public class Client : AuditableBaseEntity
+    {
+        private int _age;
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+
+        public int age
+        {
+            get
+            {
+                if (this._age <= 0)
+                {
+                    this._age = new DateTime(DateTime.Now.Subtract(this.DateOfBirth).Ticks).Year - 1;
+                }
+                return this._age;
+            }
+        }
+    }
+}
