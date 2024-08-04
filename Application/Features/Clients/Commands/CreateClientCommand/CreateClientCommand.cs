cc376d61a6a1b1170e4c4e912965c14f8bf5d1a8
JosseGonnza <accion.quevedo@hotmail.com>
2024-08-04 13:10:50 +0100

    Implemented CreateClientCommand.
    We have created the command to create clients and their exception handler and we will use the repository to store that information.

---
+++ Application/Features/Clients/Commands/CreateClientCommand/CreateClientCommand.cs
@@ -0,0 +1,23 @@
+﻿using Application.Wrappers;
+using MediatR;
+
+namespace Application.Features.Clients.Commands.CreateClientCommand
+{
+    public class CreateClientCommand : IRequest<Response<int>>
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+
+    public class CreateClientCommandHandler : IRequestHandler<CreateClientCommand, Response<int>>
+    {
+        public async Task<Response<int>> Handle(CreateClientCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
