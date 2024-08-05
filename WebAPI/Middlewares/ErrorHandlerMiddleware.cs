31e7f79a6c183b45466cfc84b1c8d7715705f284
JosseGonnza <accion.quevedo@hotmail.com>
2024-08-05 17:48:52 +0100

    Create custom ErrorMiddleware.
    We get every HTTP code to tell us what happens in the ApiRest. We take the exceptions from the Application layer and use them through the ErrorHandlerMiddleware. Finally we add it to appExtensions so we can call the class in program.cs.

---
+++ WebAPI/Middlewares/ErrorHandlerMiddleware.cs
@@ -0,0 +1,59 @@
+﻿using Application.Wrappers;
+using System.Net;
+using System.Text.Json;
+
+namespace WebAPI.Middlewares
+{
+    public class ErrorHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ErrorHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception error)
+            {
+                var response = context.Response;
+                response.ContentType = "application/json";
+
+                var responseModel = new Response<string>() { Succeeded = false, Message = error?.Message };
+
+                switch (error)
+                {
+                    //Custom Application Error
+                    case Application.Exceptions.ApiException e:
+                        response.StatusCode = (int)HttpStatusCode.BadRequest;
+                        break;
+
+                    //Custom Application Error
+                    case Application.Exceptions.ValidationException e:
+                        response.StatusCode = (int)HttpStatusCode.BadRequest;
+                        responseModel.Errors = e.Errors;
+                        break;
+
+                    // Error 404
+                    case KeyNotFoundException e:
+                        response.StatusCode = (int)HttpStatusCode.NotFound;
+                        break;
+
+                    // Unhandled Error
+                    default:
+                        response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        break;
+                }
+
+                var result = JsonSerializer.Serialize(responseModel);
+
+                await response.WriteAsync(result);
+            }
+        }
+    }
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,6 +1,7 @@
 using Application;
 using Persistence;
 using Shared;
+using WebAPI.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
     
@@ -32,6 +33,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseErrorHandlingMiddleware();
 
 app.MapControllers();
 
