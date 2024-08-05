0db7021b48deb74e802442c064d2011d908ccf1f
JosseGonnza <accion.quevedo@hotmail.com>
2024-08-05 20:02:25 +0100

    Create base controller and client controller.

---
+++ WebAPI/Controllers/v1/ClientsController.cs
@@ -0,0 +1,18 @@
+﻿using Application.Features.Clients.Commands.CreateClientCommand;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers.v1
+{
+    [ApiVersion("1.0")]
+    public class ClientsController : BaseApiController
+    {
+        // POST api/<controller>
+        [HttpPost]
+        public async Task<IActionResult> Post(CreateClientCommand command)
+        {
+            // El controlador se encarga solo de enrutar, toda la lógica se queda en el Core
+            return Ok(Mediator.Send(command));
+        }
+    }
+}
