6e47500f70ed427872822e16d3d2f13b9695481d
Ruben ZaGon-LeanMind <ruben.zamora@leanmind.es>
2024-08-06 11:54:01 +0100

    ♻️ Enhance service injection and API conventions
    Unified and expanded service injection in `ServiceExtensions` to accept additional assemblies, organizing `AddValidatorsFromAssembly` calls. Improved `BaseApiController` by introducing default API conventions and media type specifications, promoting consistent API behavior.

--- Application/ServiceExtensions.cs
+++ Application/ServiceExtensions.cs
@@ -1,15 +1,25 @@
-﻿using FluentValidation;
+﻿using System.Reflection;
+using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
-using System.Reflection;
 
 namespace Application
 {
     // Nos permite agrupar las inyecciones de nuestros servicios de terceros o los nuestros propios.
     public static class ServiceExtensions
     {
-        public static IServiceCollection AddApplicationLayer(this IServiceCollection services) 
+        public static IServiceCollection AddApplicationLayer(this IServiceCollection services, params Assembly[] otherAssemblies) 
         {
+            var currentAssembly = typeof(ServiceExtensions).Assembly;
+            services.AddValidatorsFromAssembly(currentAssembly);
+
+            var assemblies = new List<Assembly> { currentAssembly};
+        
+            if (otherAssemblies.Any())
+            {
+                assemblies.AddRange(otherAssemblies);
+            }
+            
             // Los servicios que creo dentro, pueden ser llamados desde WebAPI por AddApplicationLayer
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
             services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
