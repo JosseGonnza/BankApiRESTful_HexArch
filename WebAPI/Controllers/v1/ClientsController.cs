c877c3a5c6ff791b7e0fc9a10e7c59480feaf397
Ruben ZaGon-LeanMind <ruben.zamora@leanmind.es>
2024-08-06 11:19:50 +0100

    ♻️ Refactor ClientsController to use IMediator
    Introduced dependency injection for IMediator in ClientsController and removed the direct reference to Mediator. Also added a CancellationToken parameter to improve request handling in the Post method.

--- WebAPI/Controllers/v1/ClientsController.cs
+++ WebAPI/Controllers/v1/ClientsController.cs
@@ -1,6 +1,6 @@
 ﻿using Application.Features.Clients.Commands.CreateClientCommand;
 using Asp.Versioning;
-using Microsoft.AspNetCore.Http;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebAPI.Controllers.v1
@@ -8,12 +8,20 @@ namespace WebAPI.Controllers.v1
     [ApiVersion(Versions.V1)]
     public class ClientsController : BaseApiController
     {
+        private readonly IMediator mediator;
+
+        public ClientsController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
         // POST api/<controller>
         [HttpPost]
-        public async Task<IActionResult> Post(CreateClientCommand command)
+        public async Task<IActionResult> Post(CreateClientCommand command, CancellationToken cancellationToken)
         {
             // El controlador se encarga solo de enrutar, toda la lógica se queda en el Core
-            return Ok(Mediator.Send(command));
+            await mediator.Send(command, cancellationToken).ConfigureAwait(false);
+            return Ok();
         }
     }
 }
